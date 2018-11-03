56c41a333da31d41c19635901b7e6957cbce8e21
jeromeyangco <Jerome Yangco@JeromeYangco>
2018-11-03 11:24:57 +0800

    First Commit Upload All Files

---
+++ gitproject/src/main/java/groupIdGitProject2/gitproject/App.java
@@ -0,0 +1,13 @@
+package groupIdGitProject2.gitproject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
